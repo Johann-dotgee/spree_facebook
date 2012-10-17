7a594a171f339e2437b77038ed9e56cb49b80cf8
Johann <johann@dotgee.fr>
2012-10-17 16:06:28 +0200

    test

---
+++ app/overrides/products_facebook_direct_link_overrides.rb
@@ -0,0 +1,15 @@
+Deface::Override.new(
+  :virtual_path       => "spree/shared/_products",
+  :name               => "products_direct_link",
+  :replace            => "code[erb-loud]:contains('link_to small_image')",
+  :text               => '<%= link_to small_image(product, :itemprop => "image"), product, :itemprop => "url", "data-real" => "http://#{Spree::Config.site_url}products/#{product.permalink}" %>',
+  :disabled           => false
+)
+
+Deface::Override.new(
+  :virtual_path       => "spree/shared/_products",
+  :name               => "products_direct_link_js",
+  :insert_after       => "code[erb-silent]:contains('paginated_products.respond_to?(:num_pages)')",
+  :partial            => 'shared/direct_link_js',
+  :disabled           => false
+)
